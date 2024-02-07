936c6c7608301b76a43c206b318b0244ecf2604a
Ashley Coles <ashley@acoles.co.uk>
2024-02-07 11:00:01 +0000

    Adding the state we need to the nearest common ancestor (App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import NewsFeed from './Components/NewsFeed'
 import SideMenu from './Components/SideMenu'
 
 function App() {
-
+  const [username, setUsername] = useState('')
+  
   return (
     <>
       <Navbar />
